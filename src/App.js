d6383080b36fac2b5920f8e06820343723dfdb7c
MzAllison <isituaho@gmail.com>
2021-12-12 14:56:26 +0100

    fetch

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ function App() {
 
   // Fetch Task
 
-  const fetchTask = async () => {
+  const fetchTask = async (id) => {
     const res = await fetch(
       `http://localhost:5000/tasks/${id}`)
       const data = await res.json()
@@ -65,11 +65,25 @@ const deleteTask = async (id) => {
 }
 
 // Toggle Reminder
-const toggleReminder = (id) => {
+const toggleReminder = async (id) => {
+const taskToToggle = await fetchTask(id)
+const updTask = { ...taskToToggle,
+  reminder: !taskToToggle.reminder}
+
+  const res = await fetch( `http://localhost:5000/tasks/${id}`, {
+    method: 'PUT' ,
+    headers: {
+      'Content-type': 'application/json'
+    },
+    body: JSON.stringify(updTask)
+  })
+
+  const data = await res.json()
+
  setTasks(
    tasks.map((task) => 
    task.id === id ? {...task, reminder:
-  !task.reminder } : task 
+  data.reminder } : task 
   ))
 }
   return (
